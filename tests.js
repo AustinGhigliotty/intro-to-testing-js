440a0a3bfb11fc25f20beb3e87e64d7a27c59559
Austin Ghigliotty <yolarr@Austins-MacBook-Pro.local>
2019-06-17 16:17:05 -0500

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -13,3 +13,7 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    })});
