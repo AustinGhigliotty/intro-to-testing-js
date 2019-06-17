2e95767dbbe57390235179e1143d5756fbcd42bb
Austin Ghigliotty <yolarr@Austins-MacBook-Pro.local>
2019-06-17 16:32:51 -0500

    another commit to intro-to-testing-js

--- tests.js
+++ tests.js
@@ -16,4 +16,8 @@ describe('helloWorld', function() {
 describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
-    })});
+    });
+    it('should be a string', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    });
